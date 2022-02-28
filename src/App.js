c0b026b446dbf7a9052abb814701b182d86e5db2
qjawns0222 <33923212+qjawns0222@users.noreply.github.com>
2022-02-28 20:24:24 +0900

    1day

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import Menu from "./pages/Menu";
+import Buy from "./pages/Buy";
+import Detail from "./pages/Detail";
+import Form from "./Components/Form";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/f" element={<Form />} />
+        <Route path="/menu" element={<Menu />} />
+        <Route path="/order" element={<Buy />} />
+        <Route path="/detail:id" element={<Detail />} />
+        <Route path="*" element={<Navigate replace to="/" />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
---
+++ src/context/item.js
@@ -0,0 +1,14 @@
+const item = {
+  data: [
+    {
+      id: 0,
+      gim: 2,
+      odeng: 0,
+      addition: "ㅇㅁㄴㅇㅇㄴㅇㅁㄴㅇㅁㄴㅇㄴㅁㅇ",
+      summary: "ㅇㅁㄴㅇㅇㄴㅇㅁㄴㅇㅁ....",
+      date: "14:27",
+    },
+  ],
+  id: 0,
+};
+export default item;
